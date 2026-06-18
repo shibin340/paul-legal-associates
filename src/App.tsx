a909437f82a4ca2a1324d0e79f859245d47a593e
Shibin Joseph <shibin340@gmail.com>
2026-06-19 01:39:16 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import "./index.css";
