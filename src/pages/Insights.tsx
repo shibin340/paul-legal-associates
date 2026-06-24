a09dd1337922c3e56b972f024000a7701a90a620
Shibin Joseph <shibin340@gmail.com>
2026-06-24 13:51:10 +0530

    Removed Unused Imports

--- src/pages/Insights.tsx
+++ src/pages/Insights.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useMemo } from "react";
 import { Link } from "react-router-dom";
 import AnimateIn from "../components/ui/AnimateIn";
-import SectionHeader from "../components/ui/SectionHeader";
 import CredentialsBar from "../components/ui/CredentialsBar";
 import { ARTICLES, ARTICLE_CATEGORIES } from "../data";
 
